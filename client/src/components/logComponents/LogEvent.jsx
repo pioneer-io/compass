982e4fdeba5e7303166bbc2de08e340a7700fd3d
Kyle Ledoux <kaledoux@gmail.com>
2021-07-13 08:56:45 -0400

    log display patched; import module fixed

--- client/src/components/logComponents/LogEvent.jsx
+++ client/src/components/logComponents/LogEvent.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import parseDate from '../../lib/helpers';
+import { parseDate } from '../../lib/helpers';
 
-const LogEvent = ({flag_id, title, description, created_at}) => {
-  return (
-    <li className="py-6 sm:px-0 clear-both">
-      <h2>{title}</h2>
-      <h3>Flag ID: {flag_id}</h3>
-      <p>Description: {description}</p>
-      <p>Timestamp: {parseDate(created_at)}</p>
-    </li>
-  );
-}
+const LogEvent = ({ flag_id, title, description, created_at }) => {
+	return (
+		<li className="py-6 sm:px-0 clear-both">
+			<h2>{title}</h2>
+			<h3>Flag ID: {flag_id}</h3>
+			<p>Description: {description}</p>
+			<p>Timestamp: {parseDate(created_at)}</p>
+		</li>
+	);
+};
 
 export default LogEvent;
