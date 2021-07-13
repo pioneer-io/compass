810acc6774d03ed993e427f3ec1eac470c1a946d
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-07-13 10:54:49 -0700

    adding tests

---
+++ server/tests/controllers/flagsController.test.js
@@ -0,0 +1,13 @@
+describe("test flag controller", () => {
+  // initialize database
+  
+  afterEach(() => {
+    clearTestDb();
+  });
+
+  test("Test", () => {
+    // query the db
+  });
+
+  // drop database tables();
+});
