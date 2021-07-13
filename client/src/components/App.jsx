683699c5934be9ceb86b95b8cd8e2e5c0c6213be
Elizabeth Tackett <emctackett@gmail.com>
2021-07-13 10:10:57 -0500

    an unidentified route will display a 404

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import '../App.css';
 
 import Sidebar from './Sidebar';
 import Flags from './dashboardComponents/Flags';
 import Logs from  './logComponents/Logs';
 import SingleFlag from './flagViewComponents/SingleFlag';
+import NotFound from './errorHandling/404';
 
 function App() {
   return (
@@ -13,10 +14,13 @@ function App() {
       <div className="hidden md:flex md:flex-shrink-0 min-h-screen">
         <Sidebar />
         <div className="main w-full mx-auto sm:px-6 lg:px-8">
-          <Route path="/" exact component={Flags} />
-          <Route path="/flags" exact component={Flags} />
-          <Route path="/flags/:id" exact component={SingleFlag} />
-          <Route path="/logs" exact component={Logs} />
+          <Switch>
+            <Route path="/" exact component={Flags} />
+            <Route path="/flags" exact component={Flags} />
+            <Route path="/flags/:id" exact component={SingleFlag} />
+            <Route path="/logs" exact component={Logs} />
+            <Route path="*" component={NotFound} />
+          </Switch>
         </div>
       </div>
     </>
