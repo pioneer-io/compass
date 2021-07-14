40589774a7c27d125c9ba1e8dbe8eba5db638aa9
Elizabeth Tackett <emctackett@gmail.com>
2021-07-14 12:42:40 -0500

    add rollout to single flag view

--- client/src/components/flagViewComponents/SingleFlag.jsx
+++ client/src/components/flagViewComponents/SingleFlag.jsx
@@ -77,6 +77,14 @@ const SingleFlag = (props) => {
           </div>
           <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-500">
+              Rollout
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {flag.rollout}%
+            </dd>
+          </div>
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
               Flag ID:
             </dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
