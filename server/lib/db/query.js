1552dabb2b4f3faf6e9a0dd6cfffd4a14aae544a
Kyle Ledoux <kaledoux@gmail.com>
2021-07-14 14:08:20 -0400

    jetstream update; max messages to 1. Small log to reflect lack of params

--- server/lib/db/query.js
+++ server/lib/db/query.js
@@ -1,37 +1,36 @@
-const { Client } = require("pg");
-const HttpError = require("../../models/httpError");
+const { Client } = require('pg');
+const HttpError = require('../../models/httpError');
 
 const CLIENT_CONFIG = {
-  user: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "postgres",
-  password: "secret",
-}
-
-const logQuery = (statement, parameters) => {
-  const timeStamp = new Date();
-  const formattedTimeStamp = timeStamp.toString().substring(4, 24);
-
-  console.log(formattedTimeStamp, statement, parameters);
+	user     : 'postgres',
+	host     : 'localhost',
+	port     : 5432,
+	database : 'postgres',
+	password : 'secret'
 };
 
-const query = async(statement, parameters) => {
-  const client = new Client(CLIENT_CONFIG);
+const logQuery = (statement, parameters) => {
+	const timeStamp = new Date();
+	const formattedTimeStamp = timeStamp.toString().substring(4, 24);
 
-  await client.connect();
-  logQuery(statement, parameters);
+	console.log(formattedTimeStamp, statement, parameters || '');
+};
 
-  const result = await client.query(statement, parameters)
-    .catch(err => {
-      console.error("an error has occurred when querying the database", err);
-      throw new HttpError('An error occurred when querying the database', 500);
-    });
-  await client.end();
+const query = async (statement, parameters) => {
+	const client = new Client(CLIENT_CONFIG);
 
-  return result;
-}
+	await client.connect();
+	logQuery(statement, parameters);
+
+	const result = await client.query(statement, parameters).catch((err) => {
+		console.error('an error has occurred when querying the database', err);
+		throw new HttpError('An error occurred when querying the database', 500);
+	});
+	await client.end();
+
+	return result;
+};
 
 module.exports = {
-  query
+	query
 };
--- server/lib/nats/jetstreamManager.js
+++ server/lib/nats/jetstreamManager.js
@@ -1,50 +1,46 @@
-const { connect, AckPolicy } = require("nats");
+const { connect, AckPolicy } = require('nats');
 let nc;
 let js;
 
 async function createJetStreamConnect() {
-  nc = await connect({ servers: "localhost:4222" });
-  js = nc.jetstream();
+	nc = await connect({ servers: 'localhost:4222' });
+	js = nc.jetstream();
 }
 
 async function createStreams() {
-  await createJetStreamConnect();
-  const jsm = await nc.jetstreamManager();
+	await createJetStreamConnect();
+	const jsm = await nc.jetstreamManager();
 
-  jsm.streams.add({name: "DATA", subjects: ["DATA.*"], storage: "memory"})
-  
-  
-  const dataStreamInfo = await jsm.streams.info("DATA")
+	jsm.streams.add({ name: 'DATA', subjects: [ 'DATA.*' ], storage: 'memory', max_msgs: 1 }); //max_age: 300000000})
 
-  await addConsumers(jsm);
-  console.log("DATA stream info", dataStreamInfo)
+	const dataStreamInfo = await jsm.streams.info('DATA');
+
+	await addConsumers(jsm);
+	console.log('DATA stream info', dataStreamInfo);
 }
 
 async function addConsumers(jsm) {
-  await jsm.consumers.add('DATA', {
-    durable_name: "dataStream",
-    ack_policy: AckPolicy.Explicit,
-  });
+	await jsm.consumers.add('DATA', {
+		durable_name : 'dataStream',
+		ack_policy   : AckPolicy.Explicit
+	});
 }
 
 async function streamsCreated() {
-  await createJetStreamConnect();
-  const jsm = await nc.jetstreamManager();
+	await createJetStreamConnect();
+	const jsm = await nc.jetstreamManager();
 
-  let data;
+	let data;
 
-  try {
-    data = await jsm.streams.info("DATA")
+	try {
+		data = await jsm.streams.info('DATA');
 
-    return data.config.name === "DATA";
-  } catch(err) {
-    console.log("stream not created.", err);
-    return false;
-  }
+		return data.config.name === 'DATA';
+	} catch (err) {
+		console.log('stream not created.', err);
+		return false;
+	}
 }
 
-
-
-
 exports.createStreams = createStreams;
-exports.streamsCreated= streamsCreated;
+exports.streamsCreated = streamsCreated;
