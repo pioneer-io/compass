1c3ee711721294df810f10ae34e26f08b3b871b7
Elizabeth Tackett <emctackett@gmail.com>
2021-07-15 13:11:57 -0500

    remove LogsHeader in favor of shared, reusable component

--- client/src/components/sharedComponents/PageHead.jsx
+++ client/src/components/sharedComponents/PageHead.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const LogsHeader = () => {
+const PageHead = ({title, description}) => {
   return(
     <div className="dashboard mt-10 pb-8 border-b border-gray-200 sm:flex sm:items-center sm:justify-between">
       <div>
         <h3 className="block text-5xl leading-14 font-medium text-green-700">
-          Event Logs
+          {title}
         </h3>
-        <p className="pt-4 leading-6 block">View the logs of all flag creations, updates, and deletions.</p>
+        <p className="pt-4 leading-6 block">{description}</p>
       </div>
     </div>
   );
 };
 
-export default LogsHeader;
+export default PageHead;
