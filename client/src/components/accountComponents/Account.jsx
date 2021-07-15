5037a64de4a9df00514a4929a2fe6cc5c06ed28f
Elizabeth Tackett <emctackett@gmail.com>
2021-07-15 13:45:11 -0500

    Account page header

--- client/src/components/accountComponents/Account.jsx
+++ client/src/components/accountComponents/Account.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import PageHead from '../sharedComponents/PageHead';
 
 const Account = () => {
+  const pageDesc = 'In your application, provide the SDK key to create an SDK client connection to Scout. This will allow you to receive updated rulesets related to your feature flags.';
   return(
-    <h1>Account page!</h1>
+    <>
+      <PageHead title={'Your Account'} description={pageDesc} />
+    </>
   );
 };
 
