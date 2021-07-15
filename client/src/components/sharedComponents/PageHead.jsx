c0a3f12342fcc5962651d1414212c7106d68a7df
Elizabeth Tackett <emctackett@gmail.com>
2021-07-15 13:25:16 -0500

    get rid of FlagsHeader in favor of the shared PageHead component

--- client/src/components/sharedComponents/PageHead.jsx
+++ client/src/components/sharedComponents/PageHead.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const PageHead = ({title, description}) => {
+const isDashboard = (title) => {
+  return title === 'Feature Flags';
+}
+
+const PageHead = ({title, description, setCreatingNew}) => {
   return(
     <div className="dashboard mt-10 pb-8 border-b border-gray-200 sm:flex sm:items-center sm:justify-between">
       <div>
@@ -8,6 +12,11 @@ const PageHead = ({title, description}) => {
           {title}
         </h3>
         <p className="pt-4 leading-6 block">{description}</p>
+      </div>
+      <div className="mt-3 sm:mt-0 sm:ml-4">
+        <button onClick={() => setCreatingNew(true) }type="button" className={`${isDashboard(title) ? 'inline-flex' : 'hidden' } items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
+          Create new flag
+        </button>
       </div>
     </div>
   );
