0ce10c729d7938e452c929917a55c6224adfd52c
Kyle Ledoux <kaledoux@gmail.com>
2021-07-16 09:47:25 -0400

    test db schema change in init file; package.json test script update for port setting

--- docker/init-test.sql
+++ docker/init-test.sql
@@ -1,20 +1,25 @@
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+
 CREATE TABLE IF NOT EXISTS Flags (
   id serial,
-  title varchar(100) NOT NULL,
-  description text DEFAULT 'No description provided.',
+  title varchar(100) NOT NULL UNIQUE,
+  description text NOT NULL DEFAULT 'No description provided.',
   is_active boolean DEFAULT false NOT NULL,
   version int DEFAULT 1,
-  updated_at timestamp DEFAULT NOW(),
-  created_at timestamp DEFAULT NOW(),
-  PRIMARY KEY (id)
-);
-CREATE TABLE IF NOT EXISTS Strategies (
-  id serial,
-  flag_id int NOT NULL,
-  percentage float NOT NULL,
+  rollout int NOT NULL DEFAULT 0,
+  updated_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
+  created_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
   PRIMARY KEY (id),
-  FOREIGN KEY (flag_id) REFERENCES Flags(id) ON DELETE CASCADE
+  constraint valid_rollout CHECK (rollout >= 0 AND rollout <= 100)
 );
+
 CREATE TABLE IF NOT EXISTS Logs (
   id serial,
   flag_id int NOT NULL,
@@ -22,10 +27,10 @@ CREATE TABLE IF NOT EXISTS Logs (
   description varchar(100) NOT NULL,
   created_at timestamp DEFAULT NOW(),
   PRIMARY KEY (id)
-  -- we should preserve logs for deletions.. history will be weird otherwise
-  -- FOREIGN KEY (flag_id) REFERENCES Flags(id) ON DELETE CASCADE
 );
 
--- INSERT INTO Flags(title, description, is_active) VALUES ('LOGIN_MICROSERVICE', 'Redirects users to the login microservice', FALSE);
--- INSERT INTO Strategies(flag_id, percentage) VALUES (1, 0.1);
--- INSERT INTO Logs(flag_id, title, description) VALUES (1, 'LOGIN_MICROSERVICE', 'Created new flag: LOGIN_MICROSERVICE');
+CREATE TRIGGER tg_flags_updated_at
+  BEFORE UPDATE
+  ON flags
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_updated_at_column();
