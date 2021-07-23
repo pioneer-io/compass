81bde2c53534a90aa4e4aacde31f080465d9677f
Elizabeth Tackett <emctackett@gmail.com>
2021-07-23 13:01:02 -0500

    sdkKeysController

--- server/controllers/sdkKeysController.js
+++ server/controllers/sdkKeysController.js
@@ -8,7 +8,7 @@ const getSdkKey = async (req, res, next) => {
 		.then((key) => {
 			console.log('fetching sdk key');
 			const sdkKey = key;
-			publishSdkKey().catch(err => console.error("There was an error publishing the new sdkKey to NATS"));
+			publishSdkKey();
 			res.json({ sdkKey });
 		})
 		.catch((err) => {
