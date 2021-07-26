e43f7d44a8b6dfd67c399ecc53fd3486a98a0727
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-07-26 14:19:16 -0700

    fixed bug where forward/back button returned json

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.use('/', routes);
+app.use('/api', routes);
 
 app.use((req, res, next) => {
 	const error = new HttpError(`Could not find this route: ${req}.`, 404);
